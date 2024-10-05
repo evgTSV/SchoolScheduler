d52f8bb849d41f001a4605e675bf863c5f72461a
evgTSV <freefiery@gmail.com>
2024-10-05 13:04:19 +0500

    Preparing to start dev

--- SchoolScheduler/Program.fs
+++ SchoolScheduler/Program.fs
@@ -1,2 +1,33 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System
+open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+let webApp =
+    choose [
+        route "/" >=> redirectTo true "/ping"
+        route "/ping" >=> text "pong" ]
+
+type Startup() =
+    member _.ConfigureServices (services : IServiceCollection) =
+        services.AddGiraffe() |> ignore
+
+    member _.Configure (app : IApplicationBuilder)
+                       (env : IHostEnvironment)
+                       (loggerFactory : ILoggerFactory) =
+        app.UseGiraffe webApp
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseStartup<Startup>()
+                    |> ignore)
+        .Build()
+        .Run()
+    0
