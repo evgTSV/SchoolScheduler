1a57617c76aafda520ddf8f0bab65aa9bfb06468
evgTSV <freefiery@gmail.com>
2024-10-15 00:42:01 +0500

    Add stubs api

--- SchoolScheduler/Program.fs
+++ SchoolScheduler/Program.fs
@@ -10,11 +10,25 @@ open Microsoft.Extensions.Logging
 
 let botConfig = {
     BotToken = getEnv "BOT_TELEGRAM_TOKEN"
-    SecretToken = getEnvOr "BOT_SECRET_TOKEN" "/bot"
+    SecretToken = getEnvOr "BOT_AUTH_TOKEN" "/bot"
     Route = getEnv "BOT_HOOK_ROUTE"
     Channel = getEnv "BOT_CHANNEL_URL"
     UseFakeApi = getEnvOr "USE_FAKE_TG_API" "false" |> bool.Parse
 }    
-let mutable builder = WebApplication.CreateBuilder()
+let builder = WebApplication.CreateBuilder()
+builder.Services.AddGiraffe() |> ignore
+
+let app = builder.Build()
+
+let webApp = choose [
+    route "/" >=> text "Ok"
+    route "/config" >=> json botConfig
+]
+
+app.UseGiraffe(webApp)
+
+let server = app.RunAsync()
+
+server.Wait()
 
 ()
